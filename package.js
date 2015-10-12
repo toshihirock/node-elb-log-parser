81599248b72d45e7b054f8c50c125ae7439fce35
toshihirock <toshihiro308@gmail.com>
2015-10-12 15:44:34 +0900

    add files

---
+++ package.js
@@ -0,0 +1,24 @@
+{
+  "name": "elb-log-parser",
+  "version": "0.0.1",
+  "description": "A basic parser for ELB access logs, strongly inspired by node-clf-parser https://github.com/jfhbrook/node-clf-parser ",
+  "main": "index.js",
+  "scripts": {
+    "test": "node ./test.js"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git@github.com:toshihirock/node-elb-log-parser.git"
+  },
+  "keywords": [
+    "elb",
+    "access",
+    "log",
+    "parser"
+  ],
+  "author": "toshihirock",
+  "license": "MIT",
+  "devDependencies": {
+    "tap": "~0.4.3"
+  }
+}
